5aa7eb918b960fbc8d436e7f4852a3303492748f
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2025-03-12 13:17:48 +0700

    menambahkan class DataGeneric

--- tpmodul5_2211104027/Program.cs
+++ tpmodul5_2211104027/Program.cs
@@ -12,6 +12,10 @@ namespace tpmodul5_2211104027
         {
             // Memanggil metode generic SapaUser
             HaloGeneric.SapaUser("Fauzan");
+
+            // Menggunakan kelas DataGeneric
+            DataGeneric<string> dataGeneric = new DataGeneric<string>("NIM : 2211104027");
+            dataGeneric.PrintData();
         }
     }
 
@@ -21,6 +25,22 @@ namespace tpmodul5_2211104027
         public static void SapaUser<T>(T user)
         {
             Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    // DataGeneric.cs
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah dengan {data}");
         }
     }
 
