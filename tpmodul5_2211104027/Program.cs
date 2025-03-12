f8525f229facc3044b1820920dcb37d2e98f0a22
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2025-03-12 13:08:35 +0700

    first commit - Modul 5

---
+++ tpmodul5_2211104027/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_2211104027
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Memanggil metode generic SapaUser
+            HaloGeneric.SapaUser("Fauzan");
+        }
+    }
+
+    // HaloGeneric.cs
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+}
